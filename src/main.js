eba9ba7ba3c017a1e422fe177f1acf0114350cd4
Furkan Taşkın <furkan.taskin@tuta.io>
2023-07-21 05:58:04 +0300

    Create function to submit answers

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-const questions = {};
+let questions = [];
+let questionInstance = {};
 
 const preload = document.getElementById("preload");
 const nextButton = document.getElementById("next");
 const formWrapper = document.getElementById("wrapper");
+const quizForm = document.getElementById("quiz-form");
+const contentArea = document.getElementById("content-area");
 const timer = document.getElementById("timer");
 const questionTitle = document.getElementById("question-title");
 let questionTimer;
@@ -46,28 +49,37 @@ function fetchQuestion(getIndex = 1) {
 
 // Generate options and label as well as question
 function generateOptions(question) {
-  questionTitle.innerText = question.title;
+  // Print question and add title to the current question object
+  questionTitle.innerText = questionInstance.title = question.title;
+  questionInstance.options = {};
+
   let options = shuffleOptions(question);
 
+  // Update button text
+  if (currentQuestionIndex === 10) {
+    nextButton.innerText = "Testi Bitir";
+  }
+
+  // Update input and label values
   formWrapper.querySelectorAll("input").forEach((input, index) => {
     input.value = options[index];
+    const getId = input.id;
+    questionInstance.options[getId] = options[index];
   });
   formWrapper.querySelectorAll("label").forEach((label, index) => {
     label.innerText = options[index];
   });
 }
 
-// Init question
+// Init timer for question
 function initQuestion() {
-
   // Disable inputs
   const inputElements = document.querySelectorAll("input");
-  inputElements.forEach((input) => input.setAttribute("disabled", true));
+  // inputElements.forEach((input) => input.setAttribute("disabled", true));
 
   // Start timer
   questionTimer = setInterval(function () {
-    countdown--;
-    if (countdown <= 27) {
+    if (countdown <= 100) {
       inputElements.forEach((input) => input.removeAttribute("disabled"));
     }
 
@@ -79,33 +91,93 @@ function initQuestion() {
       clearInterval(questionTimer);
       resetCountdown();
       nextQuestion();
+    } else {
+      countdown--;
     }
   }, 1000);
 }
 
+// Reset countdown of timer
 function resetCountdown() {
   clearInterval(questionTimer);
   countdown = 30;
 }
 
+function submitAnswers() {
+  let rows = "";
+  for (let i = 0; i < questions.length; i++) {
+    let getOption = questions[i].selected;
+    let answer = getOption !== "" ? questions[i].options[getOption] : "";
+    rows += `
+    <tr class="bg-white border-b">
+        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+            ${i+1}
+        </th>
+        <td class="px-6 py-4">${questions[i].title}</td>
+        <td class="px-6 py-4">${answer !== "" ? answer : "Boş Bırakılmış"}</td>
+    </tr>`;
+  }
+
+  timer.remove();
+  contentArea.innerHTML = `
+    <table class="w-full text-sm text-center text-gray-500">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+            <tr>
+                <th scope="col" class="px-6 py-3">Soru Sayısı</th>
+                <th scope="col" class="px-6 py-3">Soru</th>
+                <th scope="col" class="px-6 py-3">Cevap</th>
+            </tr>
+        </thead>
+        <tbody>
+        ${rows}
+        </tbody>
+    </table>
+    `;
+}
+
 // Go to next question
 function nextQuestion() {
-  // Deselect input
+  // Check if any answer is selected
+  let isChecked = false;
+  let val = "";
   formWrapper.querySelectorAll("input").forEach((elem) => {
-    elem.checked = false;
+    if (elem.checked) {
+      isChecked = true;
+      val = elem.id;
+      elem.checked = false;
+    }
   });
+
+  // If user selected answer, pass the argument or pass blank
+  questionInstance["selected"] = isChecked ? val : "";
+  questions.push(questionInstance);
+  questionInstance = {};
+
+  console.log(questions);
+
+  // Reset timer
   resetCountdown();
-  currentQuestionIndex++;
-  fetchQuestion(currentQuestionIndex)
-    .then((data) => {
-      initQuestion();
-      generateOptions(data);
-    })
-    .catch((error) => {
-      console.error("Error:", error.message);
-    })
-    .finally(() => (preload.style.display = "none"));
+
+  // If user reached the last question
+  if (currentQuestionIndex === 10) {
+    submitAnswers();
+  } else {
+    // Increment question index
+    currentQuestionIndex++;
+
+    // Fetch next question
+    fetchQuestion(currentQuestionIndex)
+      .then((data) => {
+        initQuestion();
+        generateOptions(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error.message);
+      })
+      .finally(() => (preload.style.display = "none"));
+  }
 }
+
 // Init the test
 function initTest() {
   fetchQuestion(currentQuestionIndex)
@@ -120,10 +192,9 @@ function initTest() {
 }
 
 nextButton.addEventListener("click", () => {
-  nextQuestion();
+    nextQuestion();
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-  preload.style.display = "none";
   initTest();
 });
