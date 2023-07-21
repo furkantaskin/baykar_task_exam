d5eadb617cddbc26c960eb851e5ae21e17af8287
Furkan Taşkın <furkan.taskin@tuta.io>
2023-07-21 03:01:27 +0300

    Create fundamental functions

--- src/main.js
+++ src/main.js
@@ -10,8 +10,61 @@ const timer = document.getElementById("timer");
 const questionTitle = document.getElementById("question-title");
 let currentQuestionIndex = 1;
 
+// Generate array from body and shuffle content of body
+function shuffleOptions(questionData) {
+  let optionsArray = questionData.body.split("\n");
+  let currentIndex = optionsArray.length,
+    randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [optionsArray[currentIndex], optionsArray[randomIndex]] = [
+      optionsArray[randomIndex],
+      optionsArray[currentIndex],
+    ];
+  }
+  return optionsArray;
+}
+
+
+// Create input element with radio type
+function createInput() {
+  const optionInput = document.createElement("input");
+  optionInput.setAttribute("type", "radio");
+  optionInput.setAttribute("id", "ornek");
+
+  return optionInput;
+}
+
 function generateOptions(question) {
-    questionTitle.innerText = question.title;
+  questionTitle.innerText = question.title;
+  let options = shuffleOptions(question);
+  let inputCount = formWrapper.childElementCount;
+
+  for (let i = 0; i < options.length; i++) {
+    // If inputs are already created
+    if (inputCount > 0) {
+      // Create input elements with radio type
+      formWrapper.appendChild(createInput());
+    } else {
+      // Update content of inputs
+      formWrapper.querySelectorAll("input").forEach((input, index) => {
+        input.value = index;
+      });
+    }
+  }
+
+  if (inputCount > 0) {
+    for (let i = 0; i < options.length; i++) {
+      const optionInput = document.createElement("input");
+      optionInput.setAttribute("type", "radio");
+      optionInput.setAttribute("id", "ornek");
+      formWrapper.appendChild(optionInput);
+    }
+  } else {
+  }
 }
 
 // Init question
@@ -20,19 +73,23 @@ function initQuestion(question) {
 
   // Generate options from question object
   generateOptions(question);
-  
-  // Disable inpuyts
+
+  // Disable inputs
   const inputElements = document.querySelectorAll("input");
   inputElements.forEach((input) => input.setAttribute("disabled", true));
-  
+
   // Start timer
   setInterval(function () {
+    // Enable inputs after 10 seconds
     if (countdown <= 20) {
       inputElements.forEach((input) => input.removeAttribute("disabled"));
     }
+
+    // Update timer to inform user
     timer.innerText = `00:${countdown}`;
     inputElements.forEach((input) => (input.value = countdown));
 
+    // Finish the question if when reached 0
     if (countdown === 0) {
       clearInterval();
     } else {
@@ -44,14 +101,14 @@ function initQuestion(question) {
 // Go to next question
 function nextQuestion() {
   console.log("next");
+  currentQuestionIndex++;
 }
-
 
 // Go to previous question
 function prevQuestion() {
   console.log("prev");
+  currentQuestionIndex--;
 }
-
 
 // Init the test
 function initTest() {
@@ -59,6 +116,7 @@ function initTest() {
     .get("https://jsonplaceholder.typicode.com/posts/1")
     .then((response) => {
       const data = response.data;
+      console.log(data.body);
       initQuestion(data);
     })
     .catch((error) => {
@@ -70,6 +128,21 @@ function initTest() {
 }
 
 nextButton.addEventListener("click", nextQuestion);
-prevButton.addEventListener("click", prevQuestion);
+prevButton.addEventListener("click", (e) => {
+  if (currentQuestionIndex === 1) {
+    e.preventDefault();
+  } else {
+    prevQuestion();
+  }
+});
 
-document.addEventListener("DOMContentLoaded", initTest);
+document.addEventListener("DOMContentLoaded", () => {
+  if (currentQuestionIndex === 1) {
+    prevButton.style.display = "none";
+    prevButton.parentNode.style.justifyContent = "flex-end";
+  } else {
+    prevButton.removeAttribute("style");
+    prevButton.parentNode.removeAttribute("style");
+  }
+  initTest();
+});
